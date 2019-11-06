7c9261d98861e0bb6ac6026e0252e90a7db8e372
Kenny Wang <kwang@oberlin.edu>
2019-11-05 22:33:24 -0500

    * added layers to nn

--- qdlnetwork.py
+++ qdlnetwork.py
@@ -4,6 +4,7 @@ import random
 class DQLnetwork:
     def __init__(self, action_space):
         self.actions = action_space
+        self.strides = (4,2,1)
 
     def weight_var(self, shape):
         """
@@ -17,7 +18,7 @@ class DQLnetwork:
 
     def bias_var(self, shape):
         """
-        Creates a bias variable with shaope shape
+        Creates a bias variable with shape shape
         :param shape:
         :return:
         """
@@ -53,11 +54,29 @@ class DQLnetwork:
         inp_layer = tf.compat.v1.placeholder("float", [None, 128, 128, 4])
 
         #conv layer tensors
+        conv1_weight = self.weight_var([8, 8, 4, 32])
+        conv1_bias = self.bias_var([32])
+        conv2_weight = self.weight_var([4, 4, 32, 64])
+        conv2_bias = self.bias_var([64])
+        conv3_weight = self.weight_var([3, 3, 64, 64])
+        con3_bias = self.bias_var([64])
 
         #fully connected layer tensors
+        fc_weight = self.weight_var([256, 256])
+        fc_bias = self.bias_var([256])
+        output_weight = self.weight_var(256, self.actions)
+        output_bias = self.bias_var([self.actions])
 
         #output layer tensors
+        h_conv1 = tf.nn.relu(self.conv2d(inp_layer, conv1_weight, self.strides[0]) + conv1_bias)
+        h_pool1 = self.max_pool_2x2(h_conv1)
+        h_conv2 = tf.nn.relu(self.conv2d(h_pool1, conv2_weight, self.strides[1] + conv2_bias))
+        h_pool2 = self.max_pool_2x2(h_conv2)
+        h_conv3 = tf.nn.relu(self.conv2d(h_pool2, conv2_weight, self.strides[1] + conv2_bias))
+        h_pool3 = self.max_pool_2x2(h_conv3)
 
         #declaring layer declerations
+        print("I declare a layer declarations")
+
 
 
