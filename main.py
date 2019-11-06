9afccb878c43113e6e493bb82c29db3d67d3a14c
Kenny Wang <kwang@oberlin.edu>
2019-11-05 21:10:43 -0500

    * added main function and basic env

---
+++ main.py
@@ -0,0 +1,19 @@
+from nes_py.wrappers import JoypadSpace
+import gym_super_mario_bros
+from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+
+
+env = gym_super_mario_bros.make('SuperMarioBros-v0')
+env = JoypadSpace(env, SIMPLE_MOVEMENT)
+
+done = True
+for step in range(5000):
+    if done:
+        state = env.reset()
+    #Note Mario can get stuck, we should account somehow. Maybe if we dont sense forward movement, after x timesteps,
+    #end with - reward
+
+    state, reward, done, info = env.step(env.action_space.sample())
+    env.render()
+
+env.close()
